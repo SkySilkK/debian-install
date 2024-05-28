6fee91a281c7943e6c083f32b4707377dfa7b0b2
SkySilkK <79651976+SkySilkK@users.noreply.github.com>
2024-05-28 21:33:42 +0530

    feat: change default wezterm shell to zsh

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,6 +10,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.font =
   wezterm.font('FiraCode Nerd Font', { weight = 'Regular', italic = false })
 
+config.default_prog = { '/usr/bin/zsh' }
+
 config.font_size = 18 
 
 -- For example, changing the color scheme:
